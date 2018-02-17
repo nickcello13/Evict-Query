bdea521a142509d494f3663dcc6359948563d0c6
Nick Ackerman <nickackerman@nicks-mbp.fios-router.home>
2018-02-17 16:17:50 -0500

    define setup.py; ignore build output

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Evict-Query',
+    version='0.1dev',
+    packages=find_packages(),
+    license='MIT License',
+    author='Michael Pinkham, Nick Ackerman',
+    long_description=open('README.md').read()
+)
